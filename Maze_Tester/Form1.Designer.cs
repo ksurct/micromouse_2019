fd4eb91fe600d58772beafe7637d05a7dcf449a0
Nathan Helgeson <nehelgeson@gmail.com>
2018-11-14 19:47:57 -0600

    Pushing Maze Tester Project

---
+++ Maze_Tester/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace howto_maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.picMaze = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMaze)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreate.Location = new System.Drawing.Point(110, 12);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(127, 30);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Generate Maze";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // picMaze
+            // 
+            this.picMaze.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMaze.BackColor = System.Drawing.Color.White;
+            this.picMaze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picMaze.Location = new System.Drawing.Point(12, 64);
+            this.picMaze.Name = "picMaze";
+            this.picMaze.Size = new System.Drawing.Size(340, 240);
+            this.picMaze.TabIndex = 5;
+            this.picMaze.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCreate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 316);
+            this.Controls.Add(this.picMaze);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "MicroMouse Maze Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.picMaze)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.PictureBox picMaze;
+    }
+}
+
---
+++ Maze_Tester/MazeLink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace howto_maze
+{
+    class MazeLink
+    {
+        public MazeNode FromNode, ToNode;
+        public MazeLink(MazeNode from_node, MazeNode to_node)
+        {
+            FromNode = from_node;
+            ToNode = to_node;
+        }
+    }
+}
