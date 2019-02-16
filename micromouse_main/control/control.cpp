a31142738463900e43104666f8af35a7040073b1
Dylan Staatz <dms9800@gmail.com>
2019-02-15 21:20:14 -0600

    adds files from control subsystem

---
+++ micromouse_main/control/control.cpp
@@ -0,0 +1,17 @@
+/* localization.cpp */
+
+
+#include "control.h"
+
+
+/* initialize control
+ * starts the PID loop with a speed of 0 on each motor */
+void initializeControl(void) {
+    // dothething();
+}
+
+/* set speed PID
+ * Sets the speed that the control algorithm uses */
+void setSpeedPID(double left_speed, double right_speed){
+    // dothething();
+}
