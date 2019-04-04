6d610bf4114438e0ba656c3567032a44fc3f9753
Dylan Staatz <dms9800@gmail.com>
2019-04-04 02:09:06 -0500

    Reworked strategy to pass new tests

--- micromouse_main/src/strategy/strategy.cpp
+++ micromouse_main/src/strategy/strategy.cpp
@@ -7,7 +7,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <queue>
 
+
+using namespace std;
 
 /* Simple representation of a cell
  *  - For use with probabilistic_maze_t */
@@ -18,11 +21,12 @@ typedef struct {
 
 
 // Function declarations
-void floodfill(probabilistic_maze_t* robot_maze_state, cell_t* cell, int value);
+void floodfill(probabilistic_maze_t* maze_state, cell_t cell, int value);
 void convertLocationToCell(gaussian_location_t* location, cell_t* to_return);
 void convertCellToLocation(cell_t* cell, gaussian_location_t* to_return);
 cell_t chooseNextCell(probabilistic_maze_t* robot_maze_state, cell_t* robot_cell);
 void resetValues(void);
+void setAllDiscoveredToFalse(void);
 
 
 // Global declarations
@@ -34,79 +38,128 @@ static cell_t goal_cell = {
 /* The number of steps away from the goal based on the floodfill algorithm */
 int values[MAZE_WIDTH][MAZE_HEIGHT];
 
+/* Keeps track of which cells have been discovered already */
+bool discovered[MAZE_WIDTH][MAZE_HEIGHT];
+
 
 /* initialize strategy
  * Initializes the maze solving algorithm */
 void initializeStrategy(void) {
 
     resetValues();
+    setAllDiscoveredToFalse();
 }
 
 /* strategy
  * Given the robots location and the state of the maze calculate the next location to go to */
-void strategy(gaussian_location_t* robot_location, probabilistic_maze_t* robot_maze_state, gaussian_location_t* next_location) {
-    printf("check 1");
+void strategy(gaussian_location_t* robot_location, probabilistic_maze_t* maze_state, gaussian_location_t* next_location) {
+
     // Get the current cell
     cell_t robot_cell;
     convertLocationToCell(robot_location, &robot_cell);
-    printf("check 2");
-    // Rest values to MAX_VALUE
-    resetValues();
 
     // Update values by floodfill
-    floodfill(robot_maze_state, &goal_cell, 0);
+    floodfill(maze_state, goal_cell, 0);
 
     // Choose the lowest valued cell we can go to
-    cell_t next_cell = chooseNextCell(robot_maze_state, &robot_cell);
+    cell_t next_cell = chooseNextCell(maze_state, &robot_cell);
 
     // Convert to a gaussian_location_t that edits the next_location
     convertCellToLocation(&next_cell, next_location);
 }
 
+#define IS_CELL_OUT_OF_BOUNDS(cell) ((cell).x < 0 || (cell).x >= (MAZE_WIDTH) || (cell).y < 0 || (cell).y >= (MAZE_HEIGHT))
+
 /* floodfill
- * Implements the floodfill algorithm on the 2d-array values
+ * Implements the floodfill algorithm on the 2d-array values with breadth first search
  * Values should be set to numbers higher than possible to have */
-void floodfill(probabilistic_maze_t* robot_maze_state, cell_t* cell, int value) {
+void floodfill(probabilistic_maze_t* maze_state, cell_t cell, int value) {
 
-    // base case, outside of maze (shouldn't ever happen)
-    // remove if it is guaranteed that all outside walls will be > WALL_THRESHOLD
-    if (cell->x < 0 || cell->x >= MAZE_WIDTH || cell->y < 0 || cell->y >= MAZE_HEIGHT) {
-        return;
+    // Reset everything
+    resetValues();
+    setAllDiscoveredToFalse();
+
+    queue<cell_t> q;
+    queue<cell_t> next_q;
+
+    discovered[cell.x][cell.y] = true;
+
+    q.push(cell);
+
+    while (!q.empty() || !next_q.empty()) {
+
+        if (q.empty()) {
+            swap(q, next_q);
+            value++;
+        }
+
+        cell = q.front();
+        q.pop();
+
+        // Update the cell
+        if (IS_CELL_OUT_OF_BOUNDS(cell)) {
+            continue;
+        }
+        values[cell.x][cell.y] = value;
+
+        // For each direction, east(-1, 0), north(0, -1), west(1, 0), south(0, 1)
+        // if we haven't been there and we can get there, go there
+        cell_t next;
+
+        // Check North(0, -1)
+        next.x = cell.x; next.y = cell.y-1;
+        if (!IS_CELL_OUT_OF_BOUNDS(next)) {
+            if (!discovered[next.x][next.y] && maze_state->cells[cell.x][cell.y].north->exists < WALL_THRESHOLD) {
+                discovered[next.x][next.y] = true;
+                next_q.push(next);
+            }
+        }
+
+        // Check East(1, 0)
+        next.x = cell.x+1; next.y = cell.y;
+        if (!IS_CELL_OUT_OF_BOUNDS(next)) {
+            if (!discovered[next.x][next.y] && maze_state->cells[cell.x][cell.y].east->exists < WALL_THRESHOLD) {
+                discovered[next.x][next.y] = true;
+                next_q.push(next);
+            }
+        }
+
+        // Check South(0, 1)
+        next.x = cell.x; next.y = cell.y+1;
+        if (!IS_CELL_OUT_OF_BOUNDS(next)) {
+            if (!discovered[next.x][next.y] && maze_state->cells[cell.x][cell.y].south->exists < WALL_THRESHOLD) {
+                discovered[next.x][next.y] = true;
+                next_q.push(next);
+            }
+        }
+
+        // Check West(-1, 0)
+        next.x = cell.x-1; next.y = cell.y;
+        if (!IS_CELL_OUT_OF_BOUNDS(next)) {
+            if (!discovered[next.x][next.y] && maze_state->cells[cell.x][cell.y].west->exists < WALL_THRESHOLD) {
+                discovered[next.x][next.y] = true;
+                next_q.push(next);
+            }
+        }
     }
-
-    // We are at an actual cell, give it the current value if it lowers it
-    if (value < values[cell->x][cell->y])
-        values[cell->x][cell->y] = value;
-
-    // Check each direction from this cell and floodfill it
-
-    // Check North
-    if (robot_maze_state->cells[cell->x][cell->y].north->exists < WALL_THRESHOLD) {
-        cell->y--; // North one
-        floodfill(robot_maze_state, cell, value + 1);
-        cell->y++;
-    }
-
-    // Check East
-    if (robot_maze_state->cells[cell->x][cell->y].east->exists < WALL_THRESHOLD) {
-        cell->x++; // East one
-        floodfill(robot_maze_state, cell, value + 1);
-        cell->x--;
-    }
-
-    // Check South
-    if (robot_maze_state->cells[cell->x][cell->y].south->exists < WALL_THRESHOLD) {
-        cell->y++; // South one
-        floodfill(robot_maze_state, cell, value + 1);
-        cell->y--;
-    }
-
-    // Check West
-    if (robot_maze_state->cells[cell->x][cell->y].west->exists < WALL_THRESHOLD) {
-        cell->x--; // West one
-        floodfill(robot_maze_state, cell, value + 1);
-        cell->x++;
-    }
+    
+    // // Print out the maze for debugging
+    // printf("\n");
+    // for (int i=0; i<MAZE_WIDTH; i++){
+    //     for (int j=0; j<MAZE_HEIGHT; j++){
+    //         if (values[i][j] <10){
+    //             printf("  %d  |", values[i][j]);
+    //         }
+    //         else if (values[i][j] < 100){
+    //             printf(" %d  |", values[i][j]);
+    //         }
+    //         else{
+    //             printf(" %d |", values[i][j]);
+    //         }
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n"); 
 }
 
 /* Uses the mean location to determine the cell this location is in */
@@ -130,8 +183,8 @@ cell_t chooseNextCell(probabilistic_maze_t* robot_maze_state, cell_t* robot_cell
     cell_t next_cell;
 
     // Check each direction and save the lowest valued direction that we can go to
-
-    // Check North
+    
+    // Check North(0, -1)
     if (values[x][y - 1] < lowest_value && robot_maze_state->cells[x][y].north->exists < WALL_THRESHOLD) {
         // Choose North
         lowest_value = values[x][y - 1];
@@ -139,7 +192,7 @@ cell_t chooseNextCell(probabilistic_maze_t* robot_maze_state, cell_t* robot_cell
         next_cell.y = y - 1;
     }
 
-    // Check East
+    // Check East(1, 0)
     if (values[x + 1][y] < lowest_value && robot_maze_state->cells[x][y].east->exists < WALL_THRESHOLD) {
         // Choose East
         lowest_value = values[x + 1][y];
@@ -147,7 +200,7 @@ cell_t chooseNextCell(probabilistic_maze_t* robot_maze_state, cell_t* robot_cell
         next_cell.y = y;
     }
 
-    // Check South
+    // Check South(0, 1)
     if (values[x][y + 1] < lowest_value && robot_maze_state->cells[x][y].south->exists < WALL_THRESHOLD) {
         // Choose South
         lowest_value = values[x][y + 1];
@@ -155,7 +208,7 @@ cell_t chooseNextCell(probabilistic_maze_t* robot_maze_state, cell_t* robot_cell
         next_cell.y = y + 1;
     }
 
-    // Check West
+    // Check West(-1, 0)
     if (values[x - 1][y] < lowest_value && robot_maze_state->cells[x][y].west->exists < WALL_THRESHOLD) {
         // Choose West
         lowest_value = values[x - 1][y];
@@ -171,6 +224,14 @@ void resetValues(void) {
     for (int x = 0; x < MAZE_WIDTH; x++) {
         for (int y = 0; y < MAZE_HEIGHT; y++) {
             values[x][y] = MAX_VALUE;
+        }
+    }
+}
+
+void setAllDiscoveredToFalse(void){
+    for (int i=0; i<MAZE_WIDTH; i++){
+        for (int j=0; j<MAZE_HEIGHT; j++){
+            discovered[i][j] = false;
         }
     }
 }
--- micromouse_main/src/strategy/strategy_test.cpp
+++ micromouse_main/src/strategy/strategy_test.cpp
@@ -1,39 +1,209 @@
 #ifndef ARDUINO
 #include "strategy.h"
+#include "strategy_test_data.h"
 #include "../testing.h"
 #include "../settings.h"
 #include "../types.h"
 
 
+// Helper Functions
+double convertCellToLocation(int x) {
+    return (double) (((x) * (WALL_THICKNESS + CELL_LENGTH)) + (CELL_LENGTH / 2));
+}
+
+int convertLocationToCell(double x) {
+    return (int) (x / (WALL_THICKNESS + CELL_LENGTH));
+}
+
+void readInMaze(const char** maze_string, probabilistic_maze_t* maze) {
+
+    int i2 = 0;
+    for (int i = 1; i < size; i+=2) {
+        int j2 = 0;
+        for (int j = 1; j < size; j+=2) {
+            
+            // North(0, -1)
+            if (maze_string[i][j-1] == 'X') maze->cells[i2][j2].north->exists = 1.0;
+            else maze->cells[i2][j2].north->exists = 0.0;
+            
+            // East(1, 0)
+            if (maze_string[i+1][j] == 'X') maze->cells[i2][j2].east->exists = 1.0;
+            else maze->cells[i2][j2].east->exists = 0.0;
+            
+            // South(0, 1)
+            if (maze_string[i][j+1] == 'X') maze->cells[i2][j2].south->exists = 1.0;
+            else maze->cells[i2][j2].south->exists = 0.0;
+            
+            // West(-1, 0)
+            if (maze_string[i-1][j] == 'X') maze->cells[i2][j2].west->exists = 1.0;
+            else maze->cells[i2][j2].west->exists = 0.0;
+            
+            j2++;
+        }
+        i2++;
+    }
+}
+
+
 TEST_FUNC_BEGIN {
+    
     initializeStrategy();
+    TEST_PASS("initializeStrategy called");
 
-    gaussian_location_t robot_location;
-    robot_location.x.mean = 0.0;
-    robot_location.y.mean = 0.0;
-    robot_location.theta.mean = PI/2;
+    gaussian_location_t location;
+    gaussian_location_t location2;
+    probabilistic_maze_t robot_maze;
+    initializeMaze(&robot_maze);
+    int MAX_STEPS;
+    int steps;
 
-    gaussian_location_t next_location;
-    next_location.x.mean = 1.0;
-    next_location.y.mean = 0.0;
-    next_location.theta.mean = 0.0;
+    // Test empty maze
+    MAX_STEPS = MAZE_SIZE-1;
+    steps = 0;
 
-    probabilistic_maze_t robot_maze_state;
-    initializeMaze(&robot_maze_state);
+    // Setup initial location
+    location.x.mean = INIT_X_MEAN;
+    location.y.mean = INIT_Y_MEAN;
 
-    while ((next_location.x.mean != 7.0) || (next_location.y.mean != 7.0))
+    // Read in the maze
+    readInMaze(empty_string, &robot_maze);
+
+    // While not at goal
+    while (location.x.mean != (convertCellToLocation(GOAL_CELL_X)) ||
+            location.y.mean != (convertCellToLocation(GOAL_CELL_Y)))
     {
-        printf("Hello there\n");
-        printf("%f, %f\n",robot_location.x.mean, robot_location.y.mean);
-        strategy(&robot_location, &robot_maze_state, &next_location);
-        printf("General Kenobi\n");
-        robot_location = next_location;
+        // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+        // Run strategy
+        strategy(&location, &robot_maze, &location2);
+        
+        // Update location
+        location = location2;
+
+        if (++steps >= MAX_STEPS) {
+            TEST_FAIL("Empty maze solve");
+            goto after_empty_solve;
+        }
     }
 
+    // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
 
-    TEST_FAIL("no tests written yet!!!");
+    TEST_PASS("Empty maze solve");
 
+    // printf("\t took %d steps\n", steps);
+    after_empty_solve:
+    ;
 
+    // Test spiral maze
+    MAX_STEPS = 500;
+    steps = 0;
+
+    // Setup initial location
+    location.x.mean = INIT_X_MEAN;
+    location.y.mean = INIT_Y_MEAN;
+
+    // Read in the maze
+    readInMaze(spiral_string, &robot_maze);
+
+    // While not at goal
+    while (location.x.mean != (convertCellToLocation(GOAL_CELL_X)) ||
+            location.y.mean != (convertCellToLocation(GOAL_CELL_Y)))
+    {
+        // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+        // Run strategy
+        strategy(&location, &robot_maze, &location2);
+        
+        // Update location
+        location = location2;
+
+        if (++steps >= MAX_STEPS) {
+            TEST_FAIL("Spiral maze solve");
+            goto after_spiral_solve;
+        }
+    }
+
+    // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+    TEST_PASS("Spiral maze solve");
+
+    // printf("\t took %d steps\n", steps);
+    after_spiral_solve:
+    ;
+
+    // Test loop maze
+    MAX_STEPS = 500;
+    steps = 0;
+
+    // Setup initial location
+    location.x.mean = INIT_X_MEAN;
+    location.y.mean = INIT_Y_MEAN;
+
+    // Read in the maze
+    readInMaze(loop_string, &robot_maze);
+
+    // While not at goal
+    while (location.x.mean != (convertCellToLocation(GOAL_CELL_X)) ||
+            location.y.mean != (convertCellToLocation(GOAL_CELL_Y)))
+    {
+        // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+        // Run strategy
+        strategy(&location, &robot_maze, &location2);
+        
+        // Update location
+        location = location2;
+
+        if (++steps >= MAX_STEPS) {
+            TEST_FAIL("Loop maze solve");
+            goto after_loop_solve;
+        }
+    }
+
+    // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+    TEST_PASS("Loop maze solve");
+
+    // printf("\t took %d steps\n", steps);
+    after_loop_solve:
+    ;
+
+    // Test actual maze
+    MAX_STEPS = 500;
+    steps = 0;
+
+    // Setup initial location
+    location.x.mean = INIT_X_MEAN;
+    location.y.mean = INIT_Y_MEAN;
+
+    // Read in the maze
+    readInMaze(actual_string, &robot_maze);
+
+    // While not at goal
+    while (location.x.mean != (convertCellToLocation(GOAL_CELL_X)) ||
+            location.y.mean != (convertCellToLocation(GOAL_CELL_Y)))
+    {
+        // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+        // Run strategy
+        strategy(&location, &robot_maze, &location2);
+        
+        // Update location
+        location = location2;
+
+        if (++steps >= MAX_STEPS) {
+            TEST_FAIL("Actual maze solve");
+            goto after_actual_solve;
+        }
+    }
+
+    // printf("Step: %d,\tX: %d,\tY: %d\n", steps, convertLocationToCell(location.x.mean), convertLocationToCell(location.y.mean));
+
+    TEST_PASS("Actual maze solve");
+
+    // printf("\t took %d steps\n", steps);
+    after_actual_solve:
+    ;
 
 } TEST_FUNC_END("strategy_test")
 
