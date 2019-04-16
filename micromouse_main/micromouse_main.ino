fb6d8eb85fe587bf3e4ede756bc815dcb51e7489
Dylan Staatz <dms9800@gmail.com>
2019-04-16 01:36:09 -0500

    Tested turning, decent results

--- micromouse_main/micromouse_main.ino
+++ micromouse_main/micromouse_main.ino
@@ -72,7 +72,7 @@ void main_loop() {
   static double right_distance;
   static double left_speed;
   static double right_speed;
-  static gaussian_location_t next_location = {.x_mu = 263.0, .y_mu = 84.0};
+  static gaussian_location_t next_location = {.x_mu = 263.0, .y_mu = 264.0};
 
   // Get sensor data
   //readSensors(sensor_data);
